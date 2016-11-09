572b9a95fe926bb9d28e8a836f4feead0849f052
AnjithBabu <absbudget@gmail.com>
2016-11-08 21:16:18 -0800

    Added patient adding layout

--- app/src/main/java/com/chemcee/chemceecherian_comp304lab4/AddPatientDetailsActivity.java
+++ app/src/main/java/com/chemcee/chemceecherian_comp304lab4/AddPatientDetailsActivity.java
@@ -3,11 +3,11 @@ package com.chemcee.chemceecherian_comp304lab4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AddPatientDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_patient_details);
     }
 }
