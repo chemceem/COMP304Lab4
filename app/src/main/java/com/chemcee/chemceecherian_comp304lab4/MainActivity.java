fdbf0fa4ef870eaff2cc7bf759a0bdee65284f5a
chemceem <chemceem@gmail.com>
2016-11-09 02:28:21 -0500

    Redesigned the main activity

--- app/src/main/java/com/chemcee/chemceecherian_comp304lab4/MainActivity.java
+++ app/src/main/java/com/chemcee/chemceecherian_comp304lab4/MainActivity.java
@@ -1,9 +1,7 @@
 package com.chemcee.chemceecherian_comp304lab4;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void goToLogin(View view)
-    {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
     }
 }
