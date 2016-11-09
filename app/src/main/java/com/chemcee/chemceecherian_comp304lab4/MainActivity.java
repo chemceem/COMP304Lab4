5f42027d6a50bcd3df998ca5e178ed3c3b7eadb8
chemceem <chemceem@gmail.com>
2016-11-09 00:04:56 -0500

    designing the main activity. Changed button actions

--- app/src/main/java/com/chemcee/chemceecherian_comp304lab4/MainActivity.java
+++ app/src/main/java/com/chemcee/chemceecherian_comp304lab4/MainActivity.java
@@ -1,7 +1,9 @@
 package com.chemcee.chemceecherian_comp304lab4;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogin(View view)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
